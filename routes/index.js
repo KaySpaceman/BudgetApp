6a0a20e0d8368918113b74b6bc84b878a606a046
Kristers Lielups <kristers.lielups@gmail.com>
2020-02-23 12:01:25 +0200

    Config and first screen

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Budget App' });
+});
+
+router.get('/upload', function(req, res, next) {
+  res.sendFile('upload-screen.html', { title: 'Statement Upload', root: 'public' });
 });
 
 module.exports = router;
