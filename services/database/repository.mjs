e7cc8608ecd6663c6c0ccf8ab1aff7b30bd62e82
Kristers Lielups <kristers.lielups@gmail.com>
2020-04-22 19:04:20 +0300

    Only insert new transactions

--- services/database/repository.mjs
+++ services/database/repository.mjs
@@ -1,23 +1,31 @@
+import _ from 'lodash';
 import connectDb from './connector.mjs';
 
 export default function saveTransactions(data) {
   return new Promise((resolve, reject) => {
     connectDb()
       .then((db) => {
-        db.collection(process.env.MONGO_COL_TRANSACTIONS)
-          .insertMany(data)
-          .then(() => {
-            const count = data.length;
+        const collection = db.collection(process.env.MONGO_COL_TRANSACTIONS);
 
-            console.log(`Imported ${count} transactions to the DB`);
+        try {
+          const insertPromises = Object.values(data)
+            .map((entry) => collection.update(
+              { Hash: entry.Hash },
+              { $setOnInsert: entry },
+              { upsert: true },
+            ));
 
-            resolve(count);
-          })
-          .catch((reason) => {
-            console.log(`Failed to import transactions: ${reason}`);
-
-            reject(reason);
-          });
+          Promise.all(insertPromises)
+            .then((newEntries) => {
+              resolve(
+                newEntries.reduce(
+                  (count, current) => count + !_.isNil(current.result.upserted), 0,
+                ),
+              );
+            });
+        } catch (error) {
+          reject(error);
+        }
       });
   });
 }
