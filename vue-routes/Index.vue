1293d16973d402c6a629cbb4a798d69d71420a3e
Kristers Lielups <kristers.lielups@gmail.com>
2020-06-05 21:40:22 +0300

    Style Index page

--- vue-routes/Index.vue
+++ vue-routes/Index.vue
@@ -1,12 +1,14 @@
 <template>
-  <div id="app">
-    <Navigation/>
-    <div id="page-content" class="upload-section">
-      <h1>The Budget App</h1>
-      <p>Welcome to my spending and budget planing app</p>
-      <p>Author: Kristers Lielups</p>
+    <div id="app">
+        <Navigation/>
+        <div id="page-content" class="index-page">
+            <h1>Welcome to The Budget App</h1>
+            <p>Welcome to my spending and budget planing app!</p>
+            <p>Upload a CSV bank statement to add some new transactions</p>
+            <p>Author: Kristers Lielups</p>
+            <p>Repository: <a href="https://github.com/KaySpaceman/BudgetApp">GitHub</a></p>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
