5ff0f5f7e4f45641947ff2ea84b7e2bc18587776
Kristers Lielups <kristers.lielups@gmail.com>
2020-06-07 03:10:23 +0300

    Style categoy page add categoy form

--- vue-routes/Categories.vue
+++ vue-routes/Categories.vue
@@ -5,21 +5,22 @@
             <div class="page-header">
                 <h1 class="page-title">Categories</h1>
             </div>
-            <div class="cat-tree">
-                <ul class="cat-tree-subtree level-0">
-                    <CategoryTree :items="categoryTree" :level="0"/>
-                </ul>
-            </div>
-            <div class="cat-create-form-wrapper">
-                <form action="categories/new" class="cat-create-form" method="POST">
-                    <h3 class="cat-new-heading">Add a new category</h3>
-                    <label class="cat-label" for="cat-new-name">Name</label>
-                    <input id=cat-new-name type="text" name="name">
-                    <CategorySelect :categories="flatCategories" :identifier="'create'" :onlyLastLevel="false"
-                                    :label="Parent"/>
-                    <span class="cat-parent-notice">Leave empty for root category</span>
-                    <button class="cat-new-submit" type="submit">Add</button>
-                </form>
+            <div class="category-view-wrapper">
+                <div class="cat-tree">
+                    <ul class="cat-tree-subtree level-0">
+                        <CategoryTree :items="categoryTree" :level="0"/>
+                    </ul>
+                </div>
+                <div class="cat-create-form-wrapper">
+                    <form action="categories/new" class="cat-create-form" method="POST">
+                        <h2 class="cat-new-heading">Add a new category</h2>
+                        <label class="cat-new-label" for="cat-new-name">Name</label>
+                        <input id=cat-new-name type="text" name="name">
+                        <CategorySelect class="cat-new-parent-select" :categories="flatCategories"
+                                        :identifier="'create'" :onlyLastLevel="false" :label="Parent"/>
+                        <button class="cat-new-submit button" type="submit">Add</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
