a5f91a1c4a870fab7ef84bff72614f7b6d1addb6
Kristers Lielups <kristers.lielups@gmail.com>
2020-07-31 14:18:53 +0300

    Install and set up Graphql and Apollo

--- app.mjs
+++ app.mjs
@@ -1,29 +1,26 @@
 import express from 'express';
-import morgan from 'morgan';
-import cookieParser from 'cookie-parser';
-import homeRouter from './routes/home.mjs';
-import accountRouter from './routes/account.mjs';
-import bankRouter from './routes/bank.mjs';
-import transactionRouter from './routes/transactions.mjs';
-import chartRouter from './routes/charts.mjs';
-import categoryRouter from './routes/categories.mjs';
 import connectDb from './services/database/connector.mjs';
+import graphqlHttp from 'express-graphql';
+import { buildSchema } from 'graphql';
 
 const app = express();
 
 connectDb();
-app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.listen(process.env.PORT);
 
-// Routers
-app.use('/', homeRouter);
-app.use('/account', accountRouter);
-app.use('/bank', bankRouter);
-app.use('/transactions', transactionRouter);
-app.use('/charts', chartRouter);
-app.use('/categories', categoryRouter);
+app.use('/graphql', graphqlHttp({
+  schema: buildSchema(`
+    schema {
+      query:
+      mutation:
+    }
+  `),
+  rootValue: {
+
+  }
+}));
+
+app.listen(process.env.PORT);
 
 export default app;
