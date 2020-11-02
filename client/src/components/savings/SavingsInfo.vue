ff2808c97a54a65d1684a6c343f9835b6ccd8ae2
Kristers Lielups <kristers.lielups@gmail.com>
2020-11-02 19:25:24 +0200

    Inital savings info component styling

--- client/src/components/savings/SavingsInfo.vue
+++ client/src/components/savings/SavingsInfo.vue
@@ -2,19 +2,20 @@
   <div class="savings-info">
     <div class="progress">
       <!--TODO: Add progress wheel-->
+      <span class="percentage" v-text="goalPercentage"/>
     </div>
     <div class="stats">
-      <div class="unassigned">
-        <span class="title">Unassigned</span>
-        <span class="value">€ {{unassignedSavings}}</span>
+      <div class="metric unassigned">
+        <span class="name">Unassigned</span>
+        <span class="value">€ {{ unassignedSavings }}</span>
       </div>
-      <div class="stored">
-        <span class="title">Stored</span>
-        <span class="value">€ {{storedSavings}}</span>
+      <div class="metric stored">
+        <span class="name">Stored</span>
+        <span class="value">€ {{ storedSavings }}</span>
       </div>
-      <div class="goal">
-        <span class="title">Goal</span>
-        <span class="value">€ {{savingsGoal}}</span>
+      <div class="metric goal">
+        <span class="name">Goal</span>
+        <span class="value">€ {{ savingsGoal }}</span>
       </div>
     </div>
   </div>
@@ -32,6 +33,9 @@ export default {
       storedSavings: (state) => state.vaults.storedSavings,
       savingsGoal: (state) => state.vaults.savingsGoal,
     }),
+    goalPercentage() {
+      return Number.parseInt(((this.storedSavings / this.savingsGoal) * 100).toFixed(0), 10) || 0;
+    },
   },
   methods: {
     ...mapActions(['loginUser']),
@@ -39,7 +43,10 @@ export default {
   created() {
     // TODO: Remove once authentication is implemented
     if (!this.unassigned) {
-      this.loginUser({ email: 'test@test.com', password: 'test123' });
+      this.loginUser({
+        email: 'test@test.com',
+        password: 'test123',
+      });
     }
   },
 };
@@ -51,11 +58,53 @@ export default {
   border-radius: 4px;
   display: flex;
   max-width: 310px;
+  height: 60px;
   margin: 30px 0;
-  padding: 15px 20px;
+  padding: 0 15px;
+
+  .progress {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: auto 15px;
+    height: 50px;
+    width: 50px;
+
+    .percentage {
+      font-weight: $fw-extra-bold;
+      font-size: 16px;
+      line-height: 22px;
+      color: $c-charcoal;
+    }
+  }
 
   .stats {
     display: flex;
+    flex-grow: 1;
+    justify-content: flex-end;
+    align-items: center;
+
+    .metric {
+      display: flex;
+      flex-flow: column;
+      align-items: center;
+      width: 60px;
+
+      .name {
+        font-weight: $fw-semi-bold;
+        font-size: 10px;
+        line-height: 14px;
+        color: $c-cadet-blue-crayola;
+        text-transform: uppercase;
+      }
+
+      .value {
+        font-weight: $fw-semi-bold;
+        font-size: 14px;
+        line-height: 19px;
+        color: $c-dodger-blue;
+      }
+    }
   }
 }
 </style>
